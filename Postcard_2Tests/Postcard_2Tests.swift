db5660376dd0d8181179a2cb58ad07b37c98ca09
ハンザ ヒロアキ <hiroakihanza@hanza-hiroaki-no-imac-2.local>
2014-10-13 09:21:55 +0900

    Initial Commit

---
+++ Postcard_2Tests/Postcard_2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Postcard_2Tests.swift
+//  Postcard_2Tests
+//
+//  Created by ハンザ ヒロアキ on 2014/10/13.
+//  Copyright (c) 2014年 BitFountain. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Postcard_2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
